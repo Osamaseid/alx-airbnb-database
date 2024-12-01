590d9cc7da864d1ef982193c2bed264e5e977e54
osama seid <osaseid1@gmail.com>
2024-12-01 10:31:47 +0300

    subquries

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,23 +1,26 @@
--- Query to find properties with average rating > 4.0
-SELECT property_id,
-    name,
-    description,
-    pricepernight
-FROM Property
-WHERE property_id IN (
-        SELECT property_id
-        FROM Review
-        GROUP BY property_id
-        HAVING AVG(rating) > 4.0
-    );
--- Query to find users with more than 3 bookings
-SELECT user_id,
-    first_name,
-    last_name,
-    email
-FROM User
+-- Finding properties with average rating > 4.0 using a subquery:
+SELECT p.property_id,
+    p.name,
+    p.location,
+    (
+        SELECT AVG(rating)
+        FROM Review r
+        WHERE r.property_id = p.property_id
+    ) AS average_rating
+FROM Property p
+WHERE (
+        SELECT AVG(rating)
+        FROM Review r
+        WHERE r.property_id = p.property_id
+    ) > 4.0;
+-- Correlated subquery to find users with more than 3 bookings:
+SELECT u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email
+FROM User u
 WHERE (
         SELECT COUNT(*)
-        FROM Booking
-        WHERE Booking.user_id = User.user_id
+        FROM Booking b
+        WHERE b.user_id = u.user_id
     ) > 3;
