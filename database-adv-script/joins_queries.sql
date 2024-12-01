2d560e097b8b09d12d23432c5241841601a1ecbc
osama seid <osaseid1@gmail.com>
2024-12-01 22:08:19 +0300

    join

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,49 +1,36 @@
--- INNER JOIN on Booking and user
-SELECT b.booking_id,
-    b.start_date,
-    b.end_date,
-    u.user_id,
-    u.first_name,
-    u.last_name
-FROM Booking b
-    INNER JOIN User u ON b.user_id = u.user_id;
--- LEFT JOIN: Properties and Reviews
-SQL
-SELECT p.property_id,
-    p.name,
-    r.review_id,
-    r.rating,
-    r.comment
-FROM Property p
-    LEFT JOIN Review r ON p.property_id = r.property_id;
--- FULL OUTER JOIN: Users and Bookings
-SQL
-SELECT u.user_id,
-    u.first_name,
-    u.last_name,
-    b.booking_id,
-    b.start_date,
-    b.end_date
-FROM User u
-    FULL OUTER JOIN Booking b ON u.user_id = b.user_id;
--- FULL OUTER JOIN: Users and Bookings MYSQL
-SELECT u.user_id,
-    u.first_name,
-    u.last_name,
-    b.booking_id,
-    b.start_date,
-    b.end_date,
-    b.total_price
-FROM User u
-    LEFT JOIN Booking b ON u.user_id = b.user_id
-UNION
-SELECT u.user_id,
-    u.first_name,
-    u.last_name,
-    b.booking_id,
-    b.start_date,
-    b.end_date,
-    b.total_price
-FROM Booking b
-    LEFT JOIN User u ON b.user_id = u.user_id
-WHERE u.user_id IS NULL;
+-- Query 1: Retrieve all bookings and the respective users who made those bookings
+SELECT Booking.booking_id,
+    Booking.start_date,
+    Booking.end_date,
+    Booking.total_price,
+    User.user_id,
+    User.first_name,
+    User.last_name
+FROM Booking
+    INNER JOIN User ON Booking.user_id = User.user_id
+ORDER BY Booking.start_date ASC;
+-- Order by start date
+-- Query 2: Retrieve all properties and their reviews, including properties that have no reviews
+SELECT Property.property_id,
+    Property.name,
+    Property.description,
+    Review.review_id,
+    Review.rating,
+    Review.comment
+FROM Property
+    LEFT JOIN Review ON Property.property_id = Review.property_id
+ORDER BY Property.name ASC;
+-- Order by property name
+-- Query 3: Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
+SELECT User.user_id,
+    User.first_name,
+    User.last_name,
+    Booking.booking_id,
+    Booking.start_date,
+    Booking.end_date,
+    Booking.total_price
+FROM User
+    FULL OUTER JOIN Booking ON User.user_id = Booking.user_id
+ORDER BY User.last_name ASC,
+    Booking.start_date ASC;
+-- Order by user last name and booking start date
