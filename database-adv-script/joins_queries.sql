d652657bf7aae5ccd8a5de191a31e3c4d2207504
osama seid <osaseid1@gmail.com>
2024-12-01 10:08:04 +0300

    jois quries

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,29 +1,23 @@
--- Query 1: Retrieve all bookings and the respective users who made those bookings using INNER JOIN
-SELECT b.booking_id,
-    b.start_date,
-    b.end_date,
-    b.total_price,
-    u.first_name,
-    u.last_name,
-    u.email
-FROM Booking b
-    INNER JOIN User u ON b.user_id = u.user_id;
--- Query 2: Retrieve all properties and their reviews, including properties that have no reviews using LEFT JOIN
-SELECT p.property_id,
-    p.name AS property_name,
-    p.description,
-    p.pricepernight,
-    r.rating,
-    r.comment
-FROM Property p
-    LEFT JOIN Review r ON p.property_id = r.property_id;
--- Query 3: Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user using FULL OUTER JOIN
-SELECT u.user_id,
-    u.first_name,
-    u.last_name,
-    b.booking_id,
-    b.start_date,
-    b.end_date,
-    b.total_price
-FROM User u
-    FULL OUTER JOIN Booking b ON u.user_id = b.user_id;
+-- INNER JOIN: Bookings and respective users
+SELECT Booking.booking_id,
+    Booking.property_id,
+    Booking.start_date,
+    Booking.end_date,
+    Booking.total_price,
+    Booking.status,
+    User.user_id,
+    User.first_name,
+    User.last_name,
+    User.email
+FROM Booking
+    INNER JOIN User ON Booking.user_id = User.user_id;
+-- LEFT JOIN: Properties and their reviews
+SELECT Property.property_id,
+    Property.name AS property_name,
+    Property.description,
+    Property.pricepernight,
+    Review.review_id,
+    Review.rating,
+    Review.comment
+FROM Property
+    LEFT JOIN Review ON Property.property_id = Review.property_id;
